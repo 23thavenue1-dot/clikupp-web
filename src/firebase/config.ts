264a7211da1b68816c2337c30fa1f8d040ab8b0e
Jeremy Delmer <23thavenue1@gmail.com>
2025-10-30 07:42:27 +0000

    je reviens de mon site en ligne et l'upload est toujours bloquer sur cet

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9587105821-540bd",
   "appId": "1:339040917257:web:4f10eacce90f7aab4fe5d8",
-  "storageBucket": "studio-9587105821-540bd.appspot.com",
+  "storageBucket": "studio-9587105821-540bd.firebasestorage.app",
   "apiKey": "AIzaSyBQ-tigzpvson13oGkUW2Iv8RbCGLr-dEs",
   "authDomain": "studio-9587105821-540bd.firebaseapp.com",
   "measurementId": "",
