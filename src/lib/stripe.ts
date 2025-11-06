81244398000183514e22246fbf638c0f2b6fd452
Jeremy Delmer <23thavenue1@gmail.com>
2025-11-06 09:08:58 +0000

    a quel ligne doije mettre ma clef secrete test ?

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,7 +5,7 @@ import { Stripe } from 'stripe';
 
 // IMPORTANT : Pour le débogage, nous utilisons la clé secrète directement ici.
 // Remplacez "VOTRE_CLE_SECRETE_DE_TEST_ICI" par votre véritable clé secrète de test Stripe.
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY || '';
+const stripeSecretKey = "VOTRE_CLE_SECRETE_DE_TEST_ICI";
 
 export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2024-04-10',
